13c66e7630a59e76ffe47e18c7a25b8bc32cc9e1
kajj8808 <a01039388808@gmail.com>
2023-12-03 21:43:02 +0900

    add modal minsize

--- app/components/MainModal.tsx
+++ app/components/MainModal.tsx
@@ -43,13 +43,13 @@ export default function MainModal({
   };
 
   return (
-    <div className="absolute left-0 top-0 flex h-[100svh] w-full justify-center ">
+    <div className="absolute left-0 top-0 flex h-[100svh] w-full justify-center">
       <div
         className="z-30 h-full w-full bg-[rgba(0,0,0,0.7)]"
         onClick={onClick}
       ></div>
-      <div className="absolute z-50 h-full w-full max-w-5xl overflow-scroll scroll-smooth bg-white px-10 py-5 scrollbar-none sm:top-[5%] sm:h-[95%] sm:w-10/12 sm:rounded-t-3xl sm:p-10">
-        <div className="flex h-[5%] items-center justify-between">
+      <div className="fixed bottom-0 z-50 h-full min-h-[600px] w-full max-w-5xl overflow-scroll scroll-smooth bg-white px-10 py-5 scrollbar-none sm:top-[5%] sm:h-[95%] sm:w-10/12 sm:rounded-t-3xl sm:p-10">
+        <div className=" flex h-[5%] items-center justify-between">
           <div className="flex gap-1 font-bold">
             <span>[{title}]</span>
             <span>{videoId}</span>
